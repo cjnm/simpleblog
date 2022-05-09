03d04540263f9de0d9f995b2866d24c8654ae766
seejan <seejan.raj@live.com>
2022-05-09 21:31:23 +0545

    feature to replace dead cluster worker

--- api/app.js
+++ api/app.js
@@ -9,9 +9,16 @@ const { PORT } = process.env;
 
 
 if (cluster.isMaster) {
+    // Fork workers.
     for (let i = 0; i < cpuCount; i++) {
         cluster.fork();
     }
+
+    // Listen for dying workers.
+    cluster.on('exit', () => {
+        // Replace the dead worker, we're not sentimental
+        cluster.fork();
+    })
 } else {
     app.listen(PORT, () => {
         console.log(`Server is running on PID:${process.pid} port ${PORT}`);
