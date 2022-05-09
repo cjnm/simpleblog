eab3cdc4558ae05350aac5b521f6c106156e00b2
seejan <seejan.raj@live.com>
2022-05-09 21:20:39 +0545

    Implemented cluster module to run node instance on all cpu

--- api/app.js
+++ api/app.js
@@ -1,8 +1,20 @@
 import 'dotenv/config';
+import cluster from 'cluster';
+import os from 'os';
+
 import app from './src/index.js';
+const cpuCount = os.cpus().length;
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
-    console.log(`app listening on port ${PORT}`);
-})
+
+if (cluster.isMaster) {
+    for (let i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PID:${process.pid} port ${PORT}`);
+    }
+    );
+}
